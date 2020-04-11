c13665fd028cdafd27a6674e9d4fd25296237862
Phil Jerkins <Phil@TheJerkins.net>
2020-04-11 16:45:51 -0500

    Updates

---
+++ precache-manifest.c6723cd1bc3b9f9d706ca82e981bd781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa324cf18e5906ffff96d53db6f089a3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "90895eb9652ae190a6e5",
+    "url": "/react-tutorial/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "eb8ebdf890f3c74d223e",
+    "url": "/react-tutorial/static/js/2.0b8fefdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0b8fefdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90895eb9652ae190a6e5",
+    "url": "/react-tutorial/static/js/main.d1798b34.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
